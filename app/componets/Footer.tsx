9cbc2ad57df93be2334b074df8a35f6070db21da
david <davidjanp78@gmail.com>
2026-04-15 17:12:36 +0200

    fixed footer links

--- app/componets/Footer.tsx
+++ app/componets/Footer.tsx
@@ -2,11 +2,16 @@
 
 import React from "react";
 import Image from "next/image";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faYoutube, faTwitter, faPinterest, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faYoutube,
+  faTwitter,
+  faPinterest,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
-<FontAwesomeIcon icon={faFacebook} />
-
+<FontAwesomeIcon icon={faFacebook} />;
 
 const Footer = () => {
   return (
@@ -15,10 +20,9 @@ const Footer = () => {
         <div className="container mx-auto flex flex-col md:flex-row items-center md:justify-between px-4 py-10">
           <div className="font-bold text-3xl md:w-1/2 w-full" id="cta">
             <h4 className="text-white text-center md:text-left whitespace-normal md:whitespace-nowrap">
-                Irembo ry’Afurika ku Mashusho y’Ubumenyi n’Ubushakashatsi!
+              Irembo ry’Afurika ku Mashusho y’Ubumenyi n’Ubushakashatsi!
             </h4>
-            </div>
-
+          </div>
 
           <div className="w-full md:w-1/2 flex justify-center md:justify-end mt-6 md:mt-0">
             <a
@@ -36,17 +40,48 @@ const Footer = () => {
           <div className="w-full md:w-1/3 mt-10 flex flex-col items-center">
             <Image src="/logo.png" width={150} height={150} alt="logo" />
             <span className="flex space-x-4 mt-10 text-white">
-              <Image src="/facebook.svg" width={20} height={20} alt="fb" className="invert"/>
-              <Image src="/youtube.svg" width={20} height={20} alt="yt" className="invert"/>
-              <Image src="/x.svg" width={20} height={20} alt="tw" className="invert"/>
-              <Image src="/tiktok.svg" width={20} height={20} alt="pt" className="invert"/>
-              <Image src="/instagram.svg" width={20} height={20} alt="ig" className="invert" />
+              <Image
+                src="/facebook.svg"
+                width={20}
+                height={20}
+                alt="fb"
+                className="invert"
+              />
+              <Image
+                src="/youtube.svg"
+                width={20}
+                height={20}
+                alt="yt"
+                className="invert"
+              />
+              <Image
+                src="/x.svg"
+                width={20}
+                height={20}
+                alt="tw"
+                className="invert"
+              />
+              <Image
+                src="/tiktok.svg"
+                width={20}
+                height={20}
+                alt="pt"
+                className="invert"
+              />
+              <Image
+                src="/instagram.svg"
+                width={20}
+                height={20}
+                alt="ig"
+                className="invert"
+              />
             </span>
           </div>
 
           <div className="w-full md:w-1/3 flex flex-row justify-center gap-10 mt-6 md:mt-0">
             <div>
-              <ul className="text-white space-y-2 text-center md:text-left">
+              {/* Remove text-center, keep md:text-left */}
+              <ul className="text-white space-y-2 text-left md:text-left">
                 <li>
                   <a href="./" className="hover:text-amber-300">
                     Amakuru
@@ -70,7 +105,8 @@ const Footer = () => {
               </ul>
             </div>
             <div>
-              <ul className="text-white space-y-2 text-center md:text-left">
+              {/* Remove text-center, keep md:text-left */}
+              <ul className="text-white space-y-2 text-left md:text-left">
                 <li>
                   <a href="./video" className="hover:text-amber-300">
                     Videos
@@ -104,7 +140,7 @@ const Footer = () => {
               />
             </form>
             <p className="text-gray-400 text-sm pt-12">
-              RedBlue JD  &copy; 2025, All Rights Reserved
+              RedBlue JD &copy; 2025, All Rights Reserved
             </p>
           </div>
         </div>
